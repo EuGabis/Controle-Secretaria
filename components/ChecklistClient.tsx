769b285f8a4f176ad62ad1f3dbb3a7f9956d466c
Gabriel Marcelo <pereiragabriel08790@gmail.com>
2026-03-31 18:04:14 -0300

    feat: reordenamento inteligente v8.1 e dashboard restaurado no menu

--- components/ChecklistClient.tsx
+++ components/ChecklistClient.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+// v8.1 - Checklist Inteligente (Shift/Links)
+
 import { useState, useMemo, useEffect } from 'react'
 import { ChecklistItem, ChecklistTurma, ChecklistResposta } from '@/lib/types'
 import { createClient } from '@/lib/supabase/client'
